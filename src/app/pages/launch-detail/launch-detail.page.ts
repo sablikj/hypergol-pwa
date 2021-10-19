bc5feadb99b006f4872f3d960ab3f1b393529d62
Jan Sáblík <j_sablik@utb.cz>
2021-10-19 23:05:02 +0200

    Added launch details

--- src/app/pages/launch-detail/launch-detail.page.ts
+++ src/app/pages/launch-detail/launch-detail.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
+import { Launch } from 'src/app/models/launch.model';
+import { LaunchApiService } from 'src/app/services/launch-api.service';
 
 @Component({
   selector: 'app-launch-detail',
@@ -7,7 +11,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LaunchDetailPage implements OnInit {
 
-  constructor() { }
+  launch$: Observable<Launch>;
+  launch: Launch;
+
+  constructor(private apiService: LaunchApiService, private route: ActivatedRoute) {
+    const id = this.route.snapshot.paramMap.get("id");
+
+    this.launch$ = this.apiService.getUpcomingLaunch$(id);
+    this.apiService.getUpcomingLaunch$(id).subscribe(data => {
+      this.launch = data;
+    })
+
+  }
 
   ngOnInit() {
   }
